7fff814cc10751fb8a97a0029e6145845c110018
Hugh Guiney <hugh@hughguiney.com>
2020-07-07 17:01:08 -0400

    Remove Gatsby Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,12 +45,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `UA-99429830-2`,
+    //   },
+    // },
     `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
